35d8dfa0e67034f7883c2683fef714d40ac2cf36
trhiana <trhiana@rocketmail.com>
2022-08-28 12:18:37 +0000

    Created Tester.java

---
+++ CS313/Tester.java
@@ -0,0 +1,31 @@
+/*
+Rhiana Thelemaque
+CS313
+Lab 1
+*/
+
+public class Tester {
+    public static void main(String[] args) {
+        // getting the multiplication table of a number to n*12
+        Tester.multiplicationTable(24);
+
+        // getting the factorial of a number
+        int fact = Tester.factorial(5);
+        System.out.println(fact);
+    }
+
+
+    static void multiplicationTable(int n) {
+        for (int i = 1; i <= n*12; i++) {
+            int val = i * n;
+            System.out.println(String.format("%d * %d = %d", i, n, val));
+        }
+    }
+
+    static int factorial(int fact) {
+        if (fact == 0) {
+            return 1;
+        }
+        return fact * factorial(fact - 1);
+    }
+}
