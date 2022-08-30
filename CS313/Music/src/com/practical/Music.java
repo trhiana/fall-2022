28d1fea14887ebcdc4c310639c61a48a88ff304b
trhiana <trhiana@rocketmail.com>
2022-08-30 14:36:44 +0000

    Added comments

--- CS313/Music/src/com/practical/Music.java
+++ CS313/Music/src/com/practical/Music.java
@@ -31,74 +31,135 @@ public class Music {
     }
 
     /**
-     *
+     * getter for the song title / name
      * @return
      */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * setter for the song title / name
+     * @param title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * getter for the song genre
+     * @return
+     */
     public String getGenre() {
         return genre;
     }
 
+    /**
+     * setter for song genre
+     * @param genre
+     */
     public void setGenre(String genre) {
         this.genre = genre;
     }
 
+    /**
+     * getter for the song release date
+     * @return
+     */
     public String getReleaseDate() {
         return releaseDate;
     }
 
+    /**
+     * setter for the song release date
+     * @param releaseDate
+     */
     public void setReleaseDate(String releaseDate) {
         this.releaseDate = releaseDate;
     }
 
+    /**
+     * getter for the song lyrics
+     * @return
+     */
     public String getLyrics() {
         return lyrics;
     }
 
+    /**
+     * setter for the song lyrics
+     * @param lyrics
+     */
     public void setLyrics(String lyrics) {
         this.lyrics = lyrics;
     }
 
+    /**
+     * getter for the song artist
+     * @return
+     */
     public String getArtist() {
         return artist;
     }
 
+    /**
+     * setter for the song artist
+     * @param artist
+     */
     public void setArtist(String artist) {
         this.artist = artist;
     }
 
+    /**
+     * getter for the songwriter(s)
+     * @return
+     */
     public String getWriter() {
         return writer;
     }
 
+    /**
+     * setter for the songwriter(s)
+     * @param writer
+     */
     public void setWriter(String writer) {
         this.writer = writer;
     }
 
+    /**
+     * getter for the song length
+     * @return
+     */
     public double getLength() {
         return length;
     }
 
+    /**
+     * setter for the song length
+     * @param length
+     */
     public void setLength(double length) {
         this.length = length;
     }
 
+    /**
+     * getter for the song ratings
+     * @return
+     */
     public double getRatings() {
         return ratings;
     }
 
+    /**
+     * setter for the song ratings
+     * @param ratings
+     */
     public void setRatings(double ratings) {
         this.ratings = ratings;
     }
 
     /**
+     * main method
      * @param args
      */
     public static void main(String[] args) {}
