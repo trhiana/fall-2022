792bb8bb323f3cb25afd626ef6d50a5ae00a589b
trhiana <trhiana@rocketmail.com>
2022-09-07 08:14:11 +0000

    Created MATH212 for Linear Algebra

---
+++ MATH212/intro.py
@@ -0,0 +1,12 @@
+# 07th September 2022
+#  class discussion
+
+import numpy as np
+
+a = np.array([3,4,5])
+print(3*a)
+
+# creating a matrix
+a = np.array([[1,4,5], [1,1,1], [0,1,0]])
+# shape gets the size of the matrix
+print(a, a.shape)
