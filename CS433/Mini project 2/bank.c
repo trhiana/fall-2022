9426d098f8d3caa3aa760b7bc5046710e49f7594
trhiana <trhiana@rocketmail.com>
2022-10-26 15:50:21 +0000

    Edited bank.c

--- CS433/Mini project 2/bank.c
+++ CS433/Mini project 2/bank.c
@@ -2,4 +2,14 @@
 // Created by trhiana on 20/10/22.
 //
 
-int main {};
+#include "pthread.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "sys/wait.h"
+#include "unistd.h"
+
+typedef struct account {} account;
+
+typedef struct transaction {} transaction;
+
+int main( int argc, char *argv [] ) {}
